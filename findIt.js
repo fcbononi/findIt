988a275d645a290949ab5ab1fc2445a27c41cee6
unknown <charlessievers@cox.net>
2022-08-13 14:12:18 -0700

    Moving limbs into dog class

--- findIt.js
+++ findIt.js
@@ -1,12 +1,66 @@
+// dog
+class aDog {
+    constructor() {
+        this.limbs = [
+            // body
+            new dogLimb(+3,-1,100,35,"#4a3126"),
+            // tail
+            new dogLimb(+91, -9, 20, 10, "#4a3126"),
+            new dogLimb(+100, -18, 20, 10, "#4a3126"),
+            new dogLimb(+110, -30, 10, 20, "#4a3126"),
+            // legs
+            new dogLimb(+80, +33, 30, 11, "#4a3126"),
+            new dogLimb(+70, +33, 10, 11, "black"),
+            new dogLimb(+80, +44, 10, 21, "black"),
+            new dogLimb(+100, +44, 10, 21, "#4a3126"),
+            new dogLimb(+70, +64, 10, 11, "black"),
+            new dogLimb(+13, +33, 45, 11, "#4a3126"),
+            new dogLimb(-9, +44, 46, 11, "#4a3126"),
+            new dogLimb(-9, +54, 10, 11, "#4a3126"),
+            new dogLimb(+27, +54, 10, 11, "black"),
+            new dogLimb(+17, +64, 10, 11, "black"),
+            // head
+            new dogLimb(+3, -30, 32, 30, "#4a3126"),
+            new dogLimb(-7, -30, 30, 22, "#4a3126"),
+            new dogLimb(+15, -40, 20, 30, "#4a3126"),
+            new dogLimb(+25, -50, 10, 30, "#4a3126"),
+            new dogLimb(+5, -50, 10, 10, "#4a3126")
+        ]
+    }
+
+    drawDog(x, y, context) {
+        this.limbs.forEach(function(entry) {
+            entry.drawLimb(x, y, context)
+        });
+    }
+}
+
+class dogLimb {
+    constructor(x,y,w,h,color) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.color = color}
+
+    drawLimb(x, y, context) {
+        context.beginPath();
+        context.rect(x+this.x, y+this.y, this.w, this.h);
+        context.fillStyle = this.color;
+        context.fill();
+    }
+}
+
 window.onload = function() {
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
-    var x = 250;
-    var y = 150;
-    var coinx = Math.random() * (600-50);
-	var coiny = Math.random() * (400-50);
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext("2d");
+    let x = 250;
+    let y = 150;
+    let coinx = Math.random() * (600-50);
+	let coiny = Math.random() * (400-50);
+    let waffles = new aDog();
     
-    var t = Date.now();
+    let t = Date.now();
     let speed = 300;
     let dir = 0;
     let score = 0;
@@ -37,7 +91,7 @@ window.onload = function() {
     right.ontouchend = function() { dir = 0;}
 
     function draw() {
-        var timePassed = (Date.now() - t) / 1000;
+        let timePassed = (Date.now() - t) / 1000;
         t = Date.now();
 
         context.clearRect(0, 0, 600, 400);
@@ -46,79 +100,7 @@ window.onload = function() {
         context.fillStyle = 'white';
         context.fillText("I found it " + score + " times!!", 20, 40);
 
-        //dog
-        class dogObject {
-            constructor(x,y,w,h,color) {
-                this.x = x;
-                this.y = y;
-                this.w = w;
-                this.h = h;
-                this.color = color;
-            }
-            drawObject() {
-                context.beginPath();
-                context.rect(this.x, this.y, this.w, this.h);
-                context.fillStyle = this.color;
-                context.fill();
-            }
-        }
-    
-        body = new dogObject(x+3,y-1,100,35,"#4a3126");
-        body.drawObject();
-    
-        tail1 = new dogObject(x+91, y-9, 20, 10, "#4a3126");
-        tail1.drawObject();        
-    
-        tail2 = new dogObject(x+100, y-18, 20, 10, "#4a3126");
-        tail2.drawObject();
-    
-        tail3 = new dogObject(x+110, y-30, 10, 20, "#4a3126");
-        tail3.drawObject();    
-          
-        leg1 = new dogObject(x+80, y+33, 30, 11, "#4a3126");
-        leg1.drawObject();
-    
-        leg2 = new dogObject(x+70, y+33, 10, 11, "black");
-        leg2.drawObject();
-    
-        leg3 = new dogObject(x+80, y+44, 10, 21, "black");
-        leg3.drawObject();
-
-        leg4 = new dogObject(x+100, y+44, 10, 21, "#4a3126");
-        leg4.drawObject();
-
-        leg5 = new dogObject(x+70, y+64, 10, 11, "black");
-        leg5.drawObject();
-    
-        leg6 = new dogObject(x+13, y+33, 45, 11, "#4a3126");
-        leg6.drawObject();
-
-        leg7 = new dogObject(x-9, y+44, 46, 11, "#4a3126");
-        leg7.drawObject();
-
-        leg8 = new dogObject(x-9, y+54, 10, 11, "#4a3126");
-        leg8.drawObject();
-        
-        leg9 = new dogObject(x+27, y+54, 10, 11, "black");
-        leg9.drawObject();
-
-        leg10 = new dogObject(x+17, y+64, 10, 11, "black");
-        leg10.drawObject();
-
-        head1 = new dogObject(x+3, y-30, 32, 30, "#4a3126");
-        head1.drawObject();
-
-        head2 = new dogObject(x-7, y-30, 30, 22, "#4a3126");
-        head2.drawObject();
-
-        head3 = new dogObject(x+15, y-40, 20, 30, "#4a3126");
-        head3.drawObject();
-
-        head4 = new dogObject(x+25, y-50, 10, 30, "#4a3126");
-        head4.drawObject();
-
-        head5 = new dogObject(x+5, y-50, 10, 10, "#4a3126");
-        head5.drawObject();
+        waffles.drawDog(x, y, context)
 
         //tennis ball         
         context.beginPath();
@@ -139,8 +121,6 @@ window.onload = function() {
         context.strokeStyle="white";
         context.lineWidth=4;
         context.stroke();
-       
-        
 
         if(dir == 1) { 
             if(x+100 < 600) {
